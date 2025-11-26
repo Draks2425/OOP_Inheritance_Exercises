1a0bfc2f5013af637c33e9fd2175fe76a5e27c3c
Kacper Kraus <kacperkraus2425@gmail.com>
2025-11-26 12:32:37 +0100

    Add Animal base class, Dog derived class and first xUnit test for inheritance

--- OOPBase/Inheritance/Dog.cs
+++ OOPBase/Inheritance/Dog.cs
@@ -18,4 +18,3 @@ namespace OOPBase.Inheritance
         }
     }
 }
-
---
+++ OOP_Inheritance.Tests/AnimalTests.cs
@@ -0,0 +1,30 @@
+﻿using OOPBase.Inheritance;
+using Xunit;
+using System;
+
+namespace OOP_Inheritance.Tests
+{
+    public class AnimalTests
+    {
+        [Fact]
+        public void Dog_Should_MakeSound_AndShowInfo()
+        {
+            // Arrange
+            var dog = new Dog();
+            dog.Name = "Reksio";
+
+            using var writer = new StringWriter();
+            Console.SetOut(writer);
+
+            // Act
+            dog.MakeSound();
+            dog.ShowInfo();
+
+            var output = writer.ToString();
+
+            // Assert
+            Assert.Contains("Bark bark!", output);
+            Assert.Contains("Dogs name: Reksio", output);
+        }
+    }
+}
